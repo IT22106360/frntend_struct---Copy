1dd2bdb30551f183d3acdb63cd20c3e6148174ec
RaveenRanaweera <it22606488@my.sliit.lk>
2024-04-05 14:38:34 +0530

    Create Userprofile.css File

--- src/GM Dashbord/UserProfile.jsx
+++ src/GM Dashbord/UserProfile.jsx
@@ -11,30 +11,46 @@ const ClientProfile = () => {
                     
                     
                 <div className="User-box">
-                <div class="logo-container">
+                <div>
                 <div class="leftImage"></div>
-                  <div class="h5">Your Text Here</div>
+                  <div class="h5">WAVESYNC</div>
                     </div>
 
                     <div className="title">Edit Profile</div>
+
                     <form method="POST" className="add-promo">
+
+                    <div className="add-promo-row">
+                            <input
+                                type="username"
+                                id="us"
+                                name="us"
+                                className="promoInput"
+                                placeholder="  Username"
+                                required
+                            />
+                        </div>  
+
+
+
                         <div className="add-promo-row">
                             <input
                                 type="email"
                                 id="Name"
                                 name="Email"
                                 className="promoInput"
-                                placeholder="Enter Email"
+                                placeholder="  Enter Email"
                                 required
                             />
                         </div>
+
                         <div className="add-promo-row">
                             <input
                                 type="password"
                                 id="Details"
                                 name="Details"
                                 className="promoInput"
-                                placeholder="Change Password"
+                                placeholder="  Change Password"
                                 required
                             />
                         </div>
@@ -44,13 +60,13 @@ const ClientProfile = () => {
                                 id="Name"
                                 name="Email"
                                 className="promoInput"
-                                placeholder="Contact Number"
+                                placeholder="  Contact Number"
                                 required
                             />
                         </div>
                         <div className="add-promo-row">
                             <div className="add-promo-btns">
-                                <div>
+                                
                                     <button
                                         type="reset"
                                         className="secondary__btn"
@@ -59,7 +75,7 @@ const ClientProfile = () => {
                                         Update
                                     </button>
                                     {/* <button type='submit' className='primary__btn submit create-btn'>Create</button> */}
-                                </div>
+                                
                             </div>
                             {/* error - this is for me */}
                         </div>
